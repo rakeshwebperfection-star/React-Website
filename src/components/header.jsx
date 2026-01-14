4c260083333cff75c9cf36610cf5c3fe30022422
rakeshwebperfection-star <rakesh.webperfection@gmail.com>
2026-01-14 17:52:42 +0530

    Changes in Header Menu

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,7 +14,7 @@ function Header() {
                 <div className='header-menu-wrapper'>
                     <Nav className="header-menu">
                         <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#experience">Experience</Nav.Link>
+                        <Nav.Link href="#worked">Experience</Nav.Link>
                         <Nav.Link href="#work">Work</Nav.Link>
                         <Nav.Link href="#contact">Contact</Nav.Link>
                     </Nav>
